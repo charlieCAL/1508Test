6c9e5d6b68a2e57cacb2b5c553a53ee179f8e050
SQ <carriechin1003@gmail.com>
2015-12-01 10:06:32 +0800

    first

---
+++ TestGit1003/TestGit1003/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  TestGit1003
+//
+//  Created by nctarena on 15/12/1.
+//  Copyright © 2015年 CAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ TestGit1003/TestGit1003/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestGit1003
+//
+//  Created by nctarena on 15/12/1.
+//  Copyright © 2015年 CAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
